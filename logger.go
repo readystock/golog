6c6e5f3b786b5454b34904b71ab133dcfd2c14aa
Elliot Courant <elliot.courant@readystock.io>
2019-01-17 19:18:05 -0600

    Converted to spaces from tabs.

--- logger.go
+++ logger.go
@@ -1,13 +1,13 @@
 package golog
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"sync"
-	"time"
+    "fmt"
+    "io"
+    "os"
+    "sync"
+    "time"
 
-	"github.com/kataras/pio"
+    "github.com/kataras/pio"
 )
 
 // Handler is the signature type for logger's handler.
@@ -26,87 +26,87 @@ type Handler func(value *Log) (handled bool)
 
 // Logger is our golog.
 type Logger struct {
-	Prefix     []byte
-	Level      Level
-	TimeFormat string
-	// if new line should be added on all log functions, even the `F`s.
-	// It defaults to true.
-	//
-	// See `golog#NewLine(newLineChar string)` as well.
-	//
-	// Note that this will not override the time and level prefix,
-	// if you want to customize the log message please read the examples
-	// or navigate to: https://github.com/readystock/golog/issues/3#issuecomment-355895870.
-	NewLine  bool
-	mu       sync.Mutex
-	Printer  *pio.Printer
-	handlers []Handler
-	once     sync.Once
-	logs     sync.Pool
-	children *loggerMap
+    Prefix     []byte
+    Level      Level
+    TimeFormat string
+    // if new line should be added on all log functions, even the `F`s.
+    // It defaults to true.
+    //
+    // See `golog#NewLine(newLineChar string)` as well.
+    //
+    // Note that this will not override the time and level prefix,
+    // if you want to customize the log message please read the examples
+    // or navigate to: https://github.com/readystock/golog/issues/3#issuecomment-355895870.
+    NewLine  bool
+    mu       sync.Mutex
+    Printer  *pio.Printer
+    handlers []Handler
+    once     sync.Once
+    logs     sync.Pool
+    children *loggerMap
 }
 
 // New returns a new golog with a default output to `os.Stdout`
 // and level to `InfoLevel`.
 func New() *Logger {
-	return &Logger{
-		Level:      InfoLevel,
-		TimeFormat: "2006/01/02 15:04",
-		NewLine:    true,
-		Printer:    pio.NewPrinter("", os.Stdout).EnableDirectOutput().Hijack(logHijacker),
-		children:   newLoggerMap(),
-	}
+    return &Logger{
+        Level:      InfoLevel,
+        TimeFormat: "2006/01/02 15:04",
+        NewLine:    true,
+        Printer:    pio.NewPrinter("", os.Stdout).EnableDirectOutput().Hijack(logHijacker),
+        children:   newLoggerMap(),
+    }
 }
 
 // acquireLog returns a new log fom the pool.
 func (l *Logger) acquireLog(level Level, msg string, withPrintln bool) *Log {
-	log, ok := l.logs.Get().(*Log)
-	if !ok {
-		log = &Log{
-			Logger: l,
-		}
-	}
-	log.NewLine = withPrintln
-	log.Time = time.Now()
-	log.Level = level
-	log.Message = msg
-	return log
+    log, ok := l.logs.Get().(*Log)
+    if !ok {
+        log = &Log{
+            Logger: l,
+        }
+    }
+    log.NewLine = withPrintln
+    log.Time = time.Now()
+    log.Level = level
+    log.Message = msg
+    return log
 }
 
 // releaseLog Log releases a log instance back to the pool.
 func (l *Logger) releaseLog(log *Log) {
-	l.logs.Put(log)
+    l.logs.Put(log)
 }
 
 // we could use marshal inside Log but we don't have access to printer,
 // we could also use the .Handle with NopOutput too but
 // this way is faster:
 var logHijacker = func(ctx *pio.Ctx) {
-	l, ok := ctx.Value.(*Log)
-	if !ok {
-		ctx.Next()
-		return
-	}
+    l, ok := ctx.Value.(*Log)
+    if !ok {
+        ctx.Next()
+        return
+    }
 
-	line := GetTextForLevel(l.Level, ctx.Printer.IsTerminal)
-	if line != "" {
-		line += " "
-	}
+    line := GetTextForLevel(l.Level, ctx.Printer.IsTerminal)
+    if line != "" {
+        line += " "
+    }
 
-	if t := l.FormatTime(); t != "" {
-		line += t + " "
-	}
-	line += l.Message
+    if t := l.FormatTime(); t != "" {
+        line += t + " "
+    }
+    line += l.Message
 
-	var b []byte
-	if pref := l.Logger.Prefix; len(pref) > 0 {
-		b = append(pref, []byte(line)...)
-	} else {
-		b = []byte(line)
-	}
+    var b []byte
+    if pref := l.Logger.Prefix; len(pref) > 0 {
+        b = append(pref, []byte(line)...)
+    } else {
+        b = []byte(line)
+    }
 
-	ctx.Store(b, nil)
-	ctx.Next()
+    ctx.Store(b, nil)
+    ctx.Next()
 }
 
 // NopOutput disables the output.
@@ -116,8 +116,8 @@ var NopOutput = pio.NopOutput()
 //
 // Returns itself.
 func (l *Logger) SetOutput(w io.Writer) *Logger {
-	l.Printer.SetOutput(w)
-	return l
+    l.Printer.SetOutput(w)
+    return l
 }
 
 // AddOutput adds one or more `io.Writer` to the Logger's Printer.
@@ -127,8 +127,8 @@ func (l *Logger) SetOutput(w io.Writer) *Logger {
 //
 // Returns itself.
 func (l *Logger) AddOutput(writers ...io.Writer) *Logger {
-	l.Printer.AddOutput(writers...)
-	return l
+    l.Printer.AddOutput(writers...)
+    return l
 }
 
 // SetPrefix sets a prefix for this "l" Logger.
@@ -139,10 +139,10 @@ func (l *Logger) AddOutput(writers ...io.Writer) *Logger {
 //
 // Returns itself.
 func (l *Logger) SetPrefix(s string) *Logger {
-	l.mu.Lock()
-	l.Prefix = []byte(s)
-	l.mu.Unlock()
-	return l
+    l.mu.Lock()
+    l.Prefix = []byte(s)
+    l.mu.Unlock()
+    return l
 }
 
 // SetTimeFormat sets time format for logs,
@@ -150,11 +150,11 @@ func (l *Logger) SetPrefix(s string) *Logger {
 //
 // Returns itself.
 func (l *Logger) SetTimeFormat(s string) *Logger {
-	l.mu.Lock()
-	l.TimeFormat = s
-	l.mu.Unlock()
+    l.mu.Lock()
+    l.TimeFormat = s
+    l.mu.Unlock()
 
-	return l
+    return l
 }
 
 // DisableNewLine disables the new line suffix on every log function, even the `F`'s,
@@ -162,11 +162,11 @@ func (l *Logger) SetTimeFormat(s string) *Logger {
 //
 // Returns itself.
 func (l *Logger) DisableNewLine() *Logger {
-	l.mu.Lock()
-	l.NewLine = false
-	l.mu.Unlock()
+    l.mu.Lock()
+    l.NewLine = false
+    l.mu.Unlock()
 
-	return l
+    return l
 }
 
 // SetLevel accepts a string representation of
@@ -184,146 +184,146 @@ func (l *Logger) DisableNewLine() *Logger {
 //
 // Returns itself.
 func (l *Logger) SetLevel(levelName string) *Logger {
-	l.mu.Lock()
-	l.Level = fromLevelName(levelName)
-	l.mu.Unlock()
+    l.mu.Lock()
+    l.Level = fromLevelName(levelName)
+    l.mu.Unlock()
 
-	return l
+    return l
 }
 
 func (l *Logger) print(level Level, msg string, newLine bool) {
-	if l.Level >= level {
-		// newLine passed here in order for handler to know
-		// if this message derives from Println and Leveled functions
-		// or by simply, Print.
-		log := l.acquireLog(level, msg, newLine)
-		// if not handled by one of the handler
-		// then print it as usual.
-		if !l.handled(log) {
-			if newLine {
-				l.Printer.Println(log)
-			} else {
-				l.Printer.Print(log)
-			}
-		}
+    if l.Level >= level {
+        // newLine passed here in order for handler to know
+        // if this message derives from Println and Leveled functions
+        // or by simply, Print.
+        log := l.acquireLog(level, msg, newLine)
+        // if not handled by one of the handler
+        // then print it as usual.
+        if !l.handled(log) {
+            if newLine {
+                l.Printer.Println(log)
+            } else {
+                l.Printer.Print(log)
+            }
+        }
 
-		l.releaseLog(log)
-	}
-	// if level was fatal we don't care about the logger's level, we'll exit.
-	if level == FatalLevel {
-		os.Exit(1)
-	}
+        l.releaseLog(log)
+    }
+    // if level was fatal we don't care about the logger's level, we'll exit.
+    if level == FatalLevel {
+        os.Exit(1)
+    }
 }
 
 // Print prints a log message without levels and colors.
 func (l *Logger) Print(v ...interface{}) {
-	l.print(DisableLevel, fmt.Sprint(v...), l.NewLine)
+    l.print(DisableLevel, fmt.Sprint(v...), l.NewLine)
 }
 
 // Printf formats according to a format specifier and writes to `Printer#Output` without levels and colors.
 func (l *Logger) Printf(format string, args ...interface{}) {
-	l.print(DisableLevel, fmt.Sprintf(format, args...), l.NewLine)
+    l.print(DisableLevel, fmt.Sprintf(format, args...), l.NewLine)
 }
 
 // Println prints a log message without levels and colors.
 // It adds a new line at the end, it overrides the `NewLine` option.
 func (l *Logger) Println(v ...interface{}) {
-	l.print(DisableLevel, fmt.Sprint(v...), true)
+    l.print(DisableLevel, fmt.Sprint(v...), true)
 }
 
 // Log prints a leveled log message to the output.
 // This method can be used to use custom log levels if needed.
 // It adds a new line in the end.
 func (l *Logger) Log(level Level, v ...interface{}) {
-	l.print(level, fmt.Sprint(v...), l.NewLine)
+    l.print(level, fmt.Sprint(v...), l.NewLine)
 }
 
 // Logf prints a leveled log message to the output.
 // This method can be used to use custom log levels if needed.
 // It adds a new line in the end.
 func (l *Logger) Logf(level Level, format string, args ...interface{}) {
-	msg := fmt.Sprintf(format, args...)
-	l.Log(level, msg)
+    msg := fmt.Sprintf(format, args...)
+    l.Log(level, msg)
 }
 
 // Fatal `os.Exit(1)` exit no matter the level of the logger.
 // If the logger's level is fatal, error, warn, info or debug
 // then it will print the log message too.
 func (l *Logger) Fatal(v ...interface{}) {
-	l.Log(FatalLevel, v...)
+    l.Log(FatalLevel, v...)
 }
 
 // Fatalf will `os.Exit(1)` no matter the level of the logger.
 // If the logger's level is fatal, error, warn, info or debug
 // then it will print the log message too.
 func (l *Logger) Fatalf(format string, args ...interface{}) {
-	msg := fmt.Sprintf(format, args...)
-	l.Fatal(msg)
+    msg := fmt.Sprintf(format, args...)
+    l.Fatal(msg)
 }
 
 // Error will print only when logger's Level is error, warn, info or debug.
 func (l *Logger) Error(v ...interface{}) {
-	l.Log(ErrorLevel, v...)
+    l.Log(ErrorLevel, v...)
 }
 
 // Errorf will print only when logger's Level is error, warn, info or debug.
 func (l *Logger) Errorf(format string, args ...interface{}) {
-	msg := fmt.Sprintf(format, args...)
-	l.Error(msg)
+    msg := fmt.Sprintf(format, args...)
+    l.Error(msg)
 }
 
 // Warn will print when logger's Level is warn, info or debug.
 func (l *Logger) Warn(v ...interface{}) {
-	l.Log(WarnLevel, v...)
+    l.Log(WarnLevel, v...)
 }
 
 // Warnf will print when logger's Level is warn, info or debug.
 func (l *Logger) Warnf(format string, args ...interface{}) {
-	msg := fmt.Sprintf(format, args...)
-	l.Warn(msg)
+    msg := fmt.Sprintf(format, args...)
+    l.Warn(msg)
 }
 
 // Info will print when logger's Level is info or debug.
 func (l *Logger) Info(v ...interface{}) {
-	l.Log(InfoLevel, v...)
+    l.Log(InfoLevel, v...)
 }
 
 // Infof will print when logger's Level is info or debug.
 func (l *Logger) Infof(format string, args ...interface{}) {
-	msg := fmt.Sprintf(format, args...)
-	l.Info(msg)
+    msg := fmt.Sprintf(format, args...)
+    l.Info(msg)
 }
 
 // Debug will print when logger's Level is debug.
 func (l *Logger) Debug(v ...interface{}) {
-	l.Log(DebugLevel, v...)
+    l.Log(DebugLevel, v...)
 }
 
 // Debugf will print when logger's Level is debug.
 func (l *Logger) Debugf(format string, args ...interface{}) {
-	// On debug mode don't even try to fmt.Sprintf if it's not required,
-	// this can be used to allow `Debugf` to be called without even the `fmt.Sprintf`'s
-	// performance cost if the logger doesn't allow debug logging.
-	if l.Level >= DebugLevel {
-		msg := fmt.Sprintf(format, args...)
-		l.Debug(msg)
-	}
+    // On debug mode don't even try to fmt.Sprintf if it's not required,
+    // this can be used to allow `Debugf` to be called without even the `fmt.Sprintf`'s
+    // performance cost if the logger doesn't allow debug logging.
+    if l.Level >= DebugLevel {
+        msg := fmt.Sprintf(format, args...)
+        l.Debug(msg)
+    }
 }
 
 // Debug will print when logger's Level is debug.
 func (l *Logger) Verbose(v ...interface{}) {
-	l.Log(VerboseLevel, v...)
+    l.Log(VerboseLevel, v...)
 }
 
 // Debugf will print when logger's Level is debug.
 func (l *Logger) Verbosef(format string, args ...interface{}) {
-	// On debug mode don't even try to fmt.Sprintf if it's not required,
-	// this can be used to allow `Debugf` to be called without even the `fmt.Sprintf`'s
-	// performance cost if the logger doesn't allow debug logging.
-	if l.Level >= DebugLevel {
-		msg := fmt.Sprintf(format, args...)
-		l.Verbose(msg)
-	}
+    // On debug mode don't even try to fmt.Sprintf if it's not required,
+    // this can be used to allow `Debugf` to be called without even the `fmt.Sprintf`'s
+    // performance cost if the logger doesn't allow debug logging.
+    if l.Level >= DebugLevel {
+        msg := fmt.Sprintf(format, args...)
+        l.Verbose(msg)
+    }
 }
 
 // Install receives  an external logger
@@ -338,7 +338,7 @@ func (l *Logger) Verbosef(format string, args ...interface{}) {
 //
 // Look `golog#Logger.Handle` for more.
 func (l *Logger) Install(logger ExternalLogger) {
-	l.Handle(integrateExternalLogger(logger))
+    l.Handle(integrateExternalLogger(logger))
 }
 
 // InstallStd receives  a standard logger
@@ -348,13 +348,13 @@ func (l *Logger) Install(logger ExternalLogger) {
 // it can be used only once per `golog#Logger` instance.
 //
 // Example Code:
-//	import "log"
-//	myLogger := log.New(os.Stdout, "", 0)
-//	InstallStd(myLogger)
+// 	import "log"
+// 	myLogger := log.New(os.Stdout, "", 0)
+// 	InstallStd(myLogger)
 //
 // Look `golog#Logger.Handle` for more.
 func (l *Logger) InstallStd(logger StdLogger) {
-	l.Handle(integrateStdLogger(logger))
+    l.Handle(integrateStdLogger(logger))
 }
 
 // Handle adds a log handler.
@@ -370,76 +370,76 @@ func (l *Logger) InstallStd(logger StdLogger) {
 // It stops on the handler which returns true firstly.
 // The `Log` value holds the level of the print operation as well.
 func (l *Logger) Handle(handler Handler) {
-	l.mu.Lock()
-	l.handlers = append(l.handlers, handler)
-	l.mu.Unlock()
+    l.mu.Lock()
+    l.handlers = append(l.handlers, handler)
+    l.mu.Unlock()
 }
 
 func (l *Logger) handled(value *Log) (handled bool) {
-	for _, h := range l.handlers {
-		if h(value) {
-			return true
-		}
-	}
-	return false
+    for _, h := range l.handlers {
+        if h(value) {
+            return true
+        }
+    }
+    return false
 }
 
 // Hijack adds a hijacker to the low-level logger's Printer.
 // If you need to implement such as a low-level hijacker manually,
 // then you have to make use of the pio library.
 func (l *Logger) Hijack(hijacker func(ctx *pio.Ctx)) {
-	l.Printer.Hijack(hijacker)
+    l.Printer.Hijack(hijacker)
 }
 
 // Scan scans everything from "r" and prints
 // its new contents to the logger's Printer's Output,
 // forever or until the returning "cancel" is fired, once.
 func (l *Logger) Scan(r io.Reader) (cancel func()) {
-	l.once.Do(func() {
-		// add a marshaler once
-		// in order to handle []byte and string
-		// as its input.
-		// Because scan doesn't care about
-		// logging levels (because of the io.Reader)
-		// Note: We don't use the `pio.Text` built'n marshaler
-		// because we want to manage time log too.
-		l.Printer.MarshalFunc(func(v interface{}) ([]byte, error) {
-			var line []byte
-			if b, ok := v.([]byte); ok {
-				line = b
-			} else if s, ok := v.(string); ok {
-				line = []byte(s)
-			}
+    l.once.Do(func() {
+        // add a marshaler once
+        // in order to handle []byte and string
+        // as its input.
+        // Because scan doesn't care about
+        // logging levels (because of the io.Reader)
+        // Note: We don't use the `pio.Text` built'n marshaler
+        // because we want to manage time log too.
+        l.Printer.MarshalFunc(func(v interface{}) ([]byte, error) {
+            var line []byte
+            if b, ok := v.([]byte); ok {
+                line = b
+            } else if s, ok := v.(string); ok {
+                line = []byte(s)
+            }
 
-			if len(line) == 0 {
-				return nil, pio.ErrMarshalNotResponsible
-			}
+            if len(line) == 0 {
+                return nil, pio.ErrMarshalNotResponsible
+            }
 
-			formattedTime := time.Now().Format(l.TimeFormat)
-			if formattedTime != "" {
-				line = append([]byte(formattedTime+" "), line...)
-			}
+            formattedTime := time.Now().Format(l.TimeFormat)
+            if formattedTime != "" {
+                line = append([]byte(formattedTime+" "), line...)
+            }
 
-			return line, nil
-		})
-	})
+            return line, nil
+        })
+    })
 
-	return l.Printer.Scan(r, true)
+    return l.Printer.Scan(r, true)
 }
 
 // Clone returns a copy of this "l" Logger.
 // This copy is returned as pointer as well.
 func (l *Logger) Clone() *Logger {
-	return &Logger{
-		Prefix:     l.Prefix,
-		Level:      l.Level,
-		TimeFormat: l.TimeFormat,
-		Printer:    l.Printer,
-		handlers:   l.handlers,
-		children:   newLoggerMap(),
-		mu:         sync.Mutex{},
-		once:       sync.Once{},
-	}
+    return &Logger{
+        Prefix:     l.Prefix,
+        Level:      l.Level,
+        TimeFormat: l.TimeFormat,
+        Printer:    l.Printer,
+        handlers:   l.handlers,
+        children:   newLoggerMap(),
+        mu:         sync.Mutex{},
+        once:       sync.Once{},
+    }
 }
 
 // Child (creates if not exists and) returns a new child
@@ -447,40 +447,40 @@ func (l *Logger) Clone() *Logger {
 //
 // Can be used to separate logs by category.
 func (l *Logger) Child(name string) *Logger {
-	return l.children.getOrAdd(name, l)
+    return l.children.getOrAdd(name, l)
 }
 
 type loggerMap struct {
-	mu    sync.RWMutex
-	Items map[string]*Logger
+    mu    sync.RWMutex
+    Items map[string]*Logger
 }
 
 func newLoggerMap() *loggerMap {
-	return &loggerMap{
-		Items: make(map[string]*Logger),
-	}
+    return &loggerMap{
+        Items: make(map[string]*Logger),
+    }
 }
 
 func (m *loggerMap) getOrAdd(name string, parent *Logger) *Logger {
-	m.mu.RLock()
-	logger, ok := m.Items[name]
-	m.mu.RUnlock()
-	if ok {
-		return logger
-	}
+    m.mu.RLock()
+    logger, ok := m.Items[name]
+    m.mu.RUnlock()
+    if ok {
+        return logger
+    }
 
-	logger = parent.Clone()
-	prefix := name
+    logger = parent.Clone()
+    prefix := name
 
-	// if prefix doesn't end with a whitespace, then add it here.
-	if lb := name[len(prefix)-1]; lb != ' ' {
-		prefix += ": "
-	}
+    // if prefix doesn't end with a whitespace, then add it here.
+    if lb := name[len(prefix)-1]; lb != ' ' {
+        prefix += ": "
+    }
 
-	logger.SetPrefix(prefix)
-	m.mu.Lock()
-	m.Items[name] = logger
-	m.mu.Unlock()
+    logger.SetPrefix(prefix)
+    m.mu.Lock()
+    m.Items[name] = logger
+    m.mu.Unlock()
 
-	return logger
+    return logger
 }
